9cee73d6d7cd298a93b4f399a4e30bfa746fd17c
Simon Eliasson <digohd1337@gmail.com>
2015-10-06 16:21:19 +0200

    Shuttle Movement Prototype

---
+++ XcursionMars/Assets/Script/MovementScript.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementScript : MonoBehaviour {
+
+	Rigidbody rb;
+	Vector3 toApply;
+	float upThrottle = 0.04f;
+	float gravity = 0.4f;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		toApply = Vector3.zero;
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(Input.GetKey(KeyCode.D)){
+			toApply = Vector3.zero;
+			toApply.y = 1;
+			rb.AddTorque(toApply);
+		}else if(Input.GetKey(KeyCode.A)){
+			toApply = Vector3.zero;
+			toApply.y = -1;
+			rb.AddTorque(toApply);
+		}else if(Input.GetKey(KeyCode.W)){
+			toApply = Vector3.zero;
+			toApply.z = 4;
+			rb.AddRelativeForce(toApply);
+		}else if(Input.GetKey(KeyCode.S)){
+			toApply = Vector3.zero;
+			toApply.z = -4;
+			rb.AddRelativeForce(toApply);
+		}
+
+		toApply = Vector3.zero;
+		toApply.y = forceUp - gravity;
+		Debug.Log("APPLYING " + (forceUp - gravity));
+
+		Vector3 rbVelocity = rb.velocity;
+		if(rbVelocity.y > 0.5f){
+			rbVelocity.y = 0.5f;
+		}if(rbVelocity.y < -0.5f){
+			rbVelocity.y = -0.5f;
+		}
+
+		if(rbVelocity.x > 3f){
+			rbVelocity.x = 3f;
+		}if(rbVelocity.x < -3f){
+			rbVelocity.x = -3f;
+		}
+
+		if(rbVelocity.z > 3f){
+			rbVelocity.z = 3f;
+		}if(rbVelocity.z < -3f){
+			rbVelocity.z = -3f;
+		}
+		rb.velocity = rbVelocity;
+
+		rb.AddForce(toApply);
+	}
+
+	float forceUp = 0;
+
+	void OnTriggerEnter(Collider other) {;
+		Debug.Log("ENTER COL");
+	}
+
+	void OnTriggerStay(Collider other) {
+		forceUp += upThrottle;
+	}
+
+	void OnTriggerExit(Collider other) {
+		forceUp = 0;
+		Debug.Log("LEAVE COL");
+	}
+}
