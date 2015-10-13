bf0d8a77a66996594c682f800fb21d0ffef145c4
DigohD <digohd1337@gmail.com>
2015-10-13 17:05:06 +0200

    Mindless BOT implemented

--- XcursionMars/Assets/Script/BotMovement.cs
+++ XcursionMars/Assets/Script/BotMovement.cs
@@ -1,96 +1,106 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MovementScript : MonoBehaviour {
+public class BotMovement : MonoBehaviour {
 
 	Rigidbody rb;
 	Vector3 toApply;
 	float upThrottle = 0.04f;
 	float gravity = 0.4f;
 
+	bool forward, backward, left, right;
+
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody>();
 		toApply = Vector3.zero;
 	}
-
+	
 	// Update is called once per frame
 	void FixedUpdate () {
-		if(Input.GetKey(KeyCode.D)){
+		int action = Random.Range(1, 400);
+		switch(action){
+			case 1:
+				forward = true;
+				break;
+			case 2:
+				forward = true;
+				break;
+			case 3:
+				left = true;
+				break;
+			case 4:
+				right = true;
+				break;
+			case 5:
+			case 6:
+			case 7:
+			case 8:
+				forward = false;
+				backward = false;
+				left = false;
+				right = false;
+				break;
+
+		}
+
+
+		if(right){
 			toApply = Vector3.zero;
 			toApply.y = 1;
 			rb.AddTorque(toApply);
-		}else if(Input.GetKey(KeyCode.A)){
+		}else if(left){
 			toApply = Vector3.zero;
 			toApply.y = -1;
 			rb.AddTorque(toApply);
-		}else if(Input.GetKey(KeyCode.W)){
+		}else if(forward){
 			toApply = Vector3.zero;
 			toApply.z = 4;
 			rb.AddRelativeForce(toApply);
-		}else if(Input.GetKey(KeyCode.S)){
+		}else if(backward){
 			toApply = Vector3.zero;
 			toApply.z = -4;
 			rb.AddRelativeForce(toApply);
 		}
-
-		if(Input.GetAxis("Horizontal") > 0.1f){
-			toApply = Vector3.zero;
-			toApply.y = 1 * Input.GetAxis("Horizontal");
-			rb.AddTorque(toApply);
-		}else if(Input.GetAxis("Horizontal") < -0.1f){
-			toApply = Vector3.zero;
-			toApply.y = 1 * Input.GetAxis("Horizontal");
-			rb.AddTorque(toApply);
-		}
-
-		if(Input.GetAxis("Vertical") > 0.1f){
-			toApply = Vector3.zero;
-			toApply.z = 4 * Input.GetAxis("Vertical");
-			rb.AddRelativeForce(toApply);
-		}else if(Input.GetAxis("Vertical") < -0.1f){
-			toApply = Vector3.zero;
-			toApply.z = 4 * Input.GetAxis("Vertical");
-			rb.AddRelativeForce(toApply);
-		}
-
+		
 		toApply = Vector3.zero;
 		toApply.y = forceUp - gravity;
-
+		
 		Vector3 rbVelocity = rb.velocity;
 		if(rbVelocity.y > 0.5f){
 			rbVelocity.y = 0.5f;
 		}if(rbVelocity.y < -0.5f){
 			rbVelocity.y = -0.5f;
 		}
-
+		
 		if(rbVelocity.x > 3f){
 			rbVelocity.x = 3f;
 		}if(rbVelocity.x < -3f){
 			rbVelocity.x = -3f;
 		}
-
+		
 		if(rbVelocity.z > 3f){
 			rbVelocity.z = 3f;
 		}if(rbVelocity.z < -3f){
 			rbVelocity.z = -3f;
 		}
 		rb.velocity = rbVelocity;
-
+		
 		rb.AddForce(toApply);
 	}
-
+	
 	float forceUp = 0;
-
+	
 	void OnTriggerEnter(Collider other) {;
 
 	}
-
+	
 	void OnTriggerStay(Collider other) {
 		forceUp += upThrottle;
 	}
-
+	
 	void OnTriggerExit(Collider other) {
 		forceUp = 0;
+
 	}
 }
