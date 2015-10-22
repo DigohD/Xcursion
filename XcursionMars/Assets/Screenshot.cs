114a548fd42e11843e42856ba2144c79548fd4d7
Kinan Debes <gfxonline@gmail.com>
2015-10-23 01:45:07 +0200

    A scene setup specifically for screenshots
    A lot of materials, models, and prefabs are changed in this branch to
    set up a photo stage.
    Should be used only for screenshot purposes.

---
+++ XcursionMars/Assets/Screenshot.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class Screenshot : MonoBehaviour {
+
+    string rootFilename = "C:\\Users\\Kinan\\Desktop\\Screenshots\\Screenshot";
+    string extension = ".png";
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetButtonUp("Fire1"))
+        {
+            int counter = 0;
+            do
+            {
+                counter++;
+            } while (File.Exists(rootFilename + counter + extension));
+            Application.CaptureScreenshot(rootFilename + counter + extension);
+            Debug.Log("Screenshot " + rootFilename + counter + extension + " taken.");
+        }
+	}
+}
